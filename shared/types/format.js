ab061be225049a81392c59163557de086a74c921
Bryan <bryan@iflow.dev>
2026-06-09 00:33:31 +0800

    fix: remove incorrectly generated root-level files and build artifacts

--- shared/types/format.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FORMAT_TYPE_LABELS = exports.FORMAT_TYPES = void 0;
-exports.FORMAT_TYPES = ['short', 'long'];
-exports.FORMAT_TYPE_LABELS = {
-    short: '短赛',
-    long: '长赛',
-};
-//# sourceMappingURL=format.js.map
